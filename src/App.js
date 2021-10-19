0d7ab2e1e9685050dabca6fda4ed836b0d5adfa4
Napoleon Izquieta <napoleon.izquieta@gmail.com>
2021-10-19 19:58:19 +0200

    :memo: add array with words in App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,39 @@
-import './App.css';
+import "./App.css";
 
 function App() {
+  const playedWord = [
+    {
+      word: "hola",
+      solvedLetters: 0,
+      playedLetter: "",
+    },
+    {
+      word: "caracola",
+      solvedLetters: 0,
+      playedLetter: "",
+    },
+    {
+      word: "surf",
+      solvedLetters: 0,
+      playedLetter: "",
+    },
+    {
+      word: "ornitorrinco",
+      solvedLetters: 0,
+      playedLetter: "",
+    },
+    {
+      word: "perro",
+      solvedLetters: 0,
+      playedLetter: "",
+    },
+    {
+      word: "gato",
+      solvedLetters: 0,
+      playedLetter: "",
+    },
+  ];
+
   return (
     <>
       <header className="header">HANGMAN</header>
@@ -11,7 +44,13 @@ function App() {
           <line className="stage9" x1="68" y1="46" x2="56" y2="40"></line>
           <line className="stage8" x1="44" y1="46" x2="56" y2="40"></line>
           <line className="stage7" x1="56" y1="40" x2="56" y2="56"></line>
-          <circle className="stage6" cx="56" cy="34" r="6" fill="#bee5eb"></circle>
+          <circle
+            className="stage6"
+            cx="56"
+            cy="34"
+            r="6"
+            fill="#bee5eb"
+          ></circle>
           <line className="stage5" x1="56" y1="16" x2="56" y2="28"></line>
           <line className="stage4" x1="24" y1="24" x2="32" y2="16"></line>
           <line className="stage3" x1="21" y1="16" x2="60" y2="16"></line>
