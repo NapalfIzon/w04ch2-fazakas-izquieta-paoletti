81a1eedaeb1a3938e98bd4bd62a50044b6e1b98e
Rodica Fazakas <11833084+rodicafazakas@users.noreply.github.com>
2021-10-19 20:11:10 +0200

    Added Letter Component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import Letter from "./Letter.js";
 
 function App() {
   return (
@@ -31,35 +32,9 @@ function App() {
         <div className="word-container__correct">L</div>
         <div className="word-container__correct">A</div>
       </div>
-      <div className="letter-container">
-        <div className="letter">A</div>
-        <div className="letter">B</div>
-        <div className="letter">C</div>
-        <div className="letter">D</div>
-        <div className="letter">E</div>
-        <div className="letter">F</div>
-        <div className="letter">G</div>
-        <div className="letter">H</div>
-        <div className="letter">I</div>
-        <div className="letter">J</div>
-        <div className="letter">K</div>
-        <div className="letter">L</div>
-        <div className="letter">M</div>
-        <div className="letter">N</div>
-        <div className="letter">Ñ</div>
-        <div className="letter">O</div>
-        <div className="letter">P</div>
-        <div className="letter">Q</div>
-        <div className="letter">R</div>
-        <div className="letter">S</div>
-        <div className="letter">T</div>
-        <div className="letter">U</div>
-        <div className="letter">V</div>
-        <div className="letter">W</div>
-        <div className="letter">X</div>
-        <div className="letter">Y</div>
-        <div className="letter">Z</div>
-      </div>
+
+      <Letter />
+   
     </>
   );
 }
